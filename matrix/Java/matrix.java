cbcbea69ae2a2b110f1d8c49d9e0c57c2d764aab
Dario Ostuni <another.code.996@gmail.com>
2015-12-16 03:39:19 +0100

    Added matrix Java version

---
+++ matrix/Java/matrix.java
@@ -0,0 +1,41 @@
+public class matrix
+{
+	public static final int N = 512;
+	public static void main(String[] args)
+	{
+		boolean print_value = false;
+		if(args.length == 1)
+			print_value = true;
+		int seed = 42;
+		int sum = 0;
+		int[][][] matrix = new int[3][N][N];
+		for(int i = 0; i < 2; i++)
+		{
+			for(int j = 0; j < N; j++)
+			{
+				for(int k = 0; k < N; k++)
+				{
+					matrix[i][j][k] = seed;
+					seed *= 25189;
+					seed %= 32749;
+				}
+			}
+		}
+		for(int i = 0; i < N; i++)
+		{
+			for(int j = 0; j < N; j++)
+			{
+				for(int k = 0; k < N; k++)
+				{
+					matrix[2][i][j] += matrix[0][i][k] * matrix[1][k][j];
+					matrix[2][i][j] &= 0xFFFF;
+				}
+				sum += ((i * j) & 0xFF) * matrix[2][i][j];
+				sum &= 0xFFFF;
+			}
+		}
+		if(!print_value)
+			return;
+		System.out.println(sum);
+	}
+}
---
+++ matrix/Java/openjdk.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -e
+
+cd "$(dirname "$0")"
+
+case "$1" in
+
+"pre_exec")
+    javac matrix.java
+;;
+
+"exec")
+    /usr/bin/time -f %U java matrix 2> time.out
+;;
+
+"print_exec")
+    java matrix --print > print.out
+;;
+
+"version")
+    javac -version 2> version.out
+    java -version 2>> version.out
+;;
+
+"clean")
+    rm matrix.class
+    rm time.out
+    rm print.out
+    rm version.out
+;;
+
+esac
